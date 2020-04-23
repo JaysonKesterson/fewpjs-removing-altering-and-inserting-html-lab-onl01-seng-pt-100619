24633e881f38ee2d4dd99336513e3cab817d358f
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-04-23 14:54:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Write your code here!
 let main = document.querySelector("main#main");
 main.remove();
-let newHeader = document.getElementById("h1#victory");
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Inner HTML";
+document.body.appendChild(newHeader);
